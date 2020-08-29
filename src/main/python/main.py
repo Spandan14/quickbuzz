e51f9acb4be4fc045df8ec4c6d14aacb8bb5f54e
Spandan Goel <52139195+Spandan14@users.noreply.github.com>
2020-08-29 14:53:49 -0500

    added mainpage

---
+++ src/main/python/main.py
@@ -0,0 +1,21 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import sys
+from startWindow import StartWindow
+
+
+
+
+app = QApplication([])
+window = StartWindow()
+
+sys.exit(app.exec_())
+
+
+
+
+app.exec_()
+
+# https://coolors.co/f9ada0-fcf6bd-d0f4de-a9def9-e4c1f9 - colors
+
