f90527bf535a8e474e99f56e6ecf71ae36ede3e1
Spandan Goel <52139195+Spandan14@users.noreply.github.com>
2020-08-30 17:01:14 -0500

    finished upto difficulties

--- src/main/python/classes.py
+++ src/main/python/classes.py
@@ -159,7 +159,7 @@ class InfoWindow(QMainWindow):
 
 # categories and subcategories for api and ui
 categories = ["Current Events", "Fine Arts", "Geography", "History", "Literature", "Mythology", "Philosophy", "Religion"
-              , "Science", "Social Science", "Trash", "Everything"]
+              , "Science", "Social Studies", "Trash", "Everything"]
 categoriesID = [26, 21, 20, 18, 15, 14, 25, 19, 17, 22, 16, -1]
 subcategories = ["Current Events American", "Current Events Other", "Fine Arts American", "Fine Arts Audiovisual"
                  , "Fine Arts Auditory", "Fine Arts British", "Fine Arts European", "Fine Arts Opera", "Fine Arts Other"
@@ -173,9 +173,9 @@ subcategories = ["Current Events American", "Current Events Other", "Fine Arts A
                  , "Religion American", "Religion Christianity", "Religion East Asian", "Religion Islam"
                  , "Religion Judaism", "Religion Other", "Science American", "Science Biology", "Science Chemistry"
                  , "Science Computer Science", "Science Math", "Science Other", "Science Physics", "Science World"
-                 , "Social Science American", "Social Science Anthropology", "Social Science Economics"
-                 , "Social Science Linguistics", "Social Science Other", "Social Science Political Science"
-                 , "Social Science Psychology", "Social Science Sociology", "Trash American", "Trash Movies"
+                 , "Social Studies American", "Social Studies Anthropology", "Social Studies Economics"
+                 , "Social Studies Linguistics", "Social Studies Other", "Social Studies Political Science"
+                 , "Social Studies Psychology", "Social Studies Sociology", "Trash American", "Trash Movies"
                  , "Trash Music", "Trash Other", "Trash Sports", "Trash Television", "Trash Video Games"
                  , "Everything"]
 subcategoriesID = [40, 42, 35, 27, 8, 45, 50, 77, 25, 2, 43, 38, 44, 13, 6, 16, 24, 28, 20, 4, 22, 30, 1, 29, 12, 33
@@ -189,6 +189,8 @@ selectedCategories = []
 selectedCategoryButtons = []
 selectedSubCategories = []
 selectedSubCategoryButtons = []
+selectedDifficulties = []
+selectedDifficultiesButtons = []
 
 
 class CategorySelection(QMainWindow):
@@ -421,7 +423,6 @@ class SubCategorySelection(QMainWindow):
 
         # dropdowns for category and subcategory
 
-
         self.subCategoryDrop = QComboBox(self)
         if "Everything" in selectedCategories:
             self.subCategoryDrop.addItem("Everything")
@@ -432,17 +433,15 @@ class SubCategorySelection(QMainWindow):
                         self.subCategoryDrop.addItem(j)
                 self.subCategoryDrop.addItem("Everything " + i)
 
-
-        self.subCategoryDropLabel = QLabel("Choose a category")
         self.subCategoryDrop.activated[str].connect(self.dropDownChangedSubCategory)
         self.subCategoryDrop.setFont(QFont("Helvetica Neue", 14))
         self.subCategoryDrop.setFixedWidth(370)
         self.subCategoryDrop.setStyleSheet("border: 5px black; background: #E4C1F9; height: 50px;")
 
         self.rmSubCat = QPushButton()
-        self.rmSubCat.setText("Remove Category")
+        self.rmSubCat.setText("Remove Subcategory")
         self.rmSubCat.setFont(QFont('Helvetica Neue', 12))
-        self.rmSubCat.setFixedWidth(200)
+        self.rmSubCat.setFixedWidth(250)
         self.rmSubCat.setStyleSheet("QPushButton {"
                                  "background-color: #FCF6BD;"
                                  "border-radius: 15px; "
@@ -456,7 +455,7 @@ class SubCategorySelection(QMainWindow):
                                  "padding:5px;"
                                  "border-style: inset;"
                                  "}")
-
+        self.rmSubCat.clicked.connect(self.removeSubCat)
         # navigation buttons
         self.backButton = QPushButton()
         self.backButton.setText("Back")
@@ -520,13 +519,219 @@ class SubCategorySelection(QMainWindow):
         self.mainLayout.addLayout(self.thirdHor)
         self.setCentralWidget(self.widget)
 
-    def dropDownChangedCategory(self, text):
-        self.subCategoryDrop.removeItem(text)
-        if "Everything" in text:
+    def dropDownChangedSubCategory(self, text):
+        self.subCategoryDrop.setPlaceholderText(text)
+        if "Everything" not in text:
             temp = text.split(" ")
-            for i in subcategories:
-                if temp[1] in i:
+            for i in range(self.subCategoryDrop.count()):
+                if "Everything" in self.subCategoryDrop.itemText(i) and temp[0] in self.subCategoryDrop.itemText(i):
                     self.subCategoryDrop.removeItem(i)
+
+
+
+
+        selectedSubCategories.append(text)
+
+        self.tempButton = QPushButton()
+        self.tempButton.setText(text)
+        self.tempButton.setFont(QFont('Helvetica Neue', 4))
+        self.tempButton.setFixedWidth(100)
+        self.tempButton.setStyleSheet("QPushButton {"
+                                      "background-color: #D0F4DE;"
+                                      "border-radius: 15px; "
+                                      "padding: 5px; "
+                                      "border-style: outset;"
+                                      "margin: 5px"
+                                      "}"
+                                      "QPushButton:pressed {"
+                                      "background-color: #A9DEF9;"
+                                      "border-radius: 15px;"
+                                      "padding:5px;"
+                                      "border-style: inset;"
+                                      "}")
+        self.secondHor.addWidget(self.tempButton, self.gridx, self.gridy)
+        print(self.gridx)
+        print(self.gridy)
+        self.gridy += 1
+        if self.gridy % 5 == 0 and self.gridy > 0:
+            self.gridx += 1
+            self.gridy = self.gridy % 5
+        selectedSubCategoryButtons.append(self.tempButton)
+
+    def removeSubCat(self):
+        try:
+
+
+
+
+            selectedSubCategoryButtons[len(selectedSubCategoryButtons)-1].hide()
+            selectedSubCategoryButtons.pop()
+            selectedSubCategories.pop()
+            if self.gridy != 0:
+                self.gridy -= 1
+            elif self.gridy == 0 and self.gridx != 0:
+                self.gridx -= 1
+                self.gridy = 4
+
+
+        except:
+
+            catReply = QMessageBox()
+            catReply.setIcon(QMessageBox.Warning)
+            catReply.setWindowTitle("QuickBuzz")
+            catReply.setText("There are no subcategories to remove")
+            catReply.setStandardButtons(QMessageBox.Ok)
+
+            returnValue = catReply.exec()
+            if returnValue == QMessageBox.Ok:
+                print("ok")
+
+        availableSubcats = []
+        for j in range(self.subCategoryDrop.count()):
+            availableSubcats.append(self.subCategoryDrop.itemText(j))
+        for i in selectedCategories:
+            addevery = True
+            everytemp = "Everything " + i
+            if everytemp not in availableSubcats:
+                for j in selectedSubCategories:
+                    if i in j:
+                        addevery = False
+                if addevery:
+                    self.subCategoryDrop.addItem(everytemp)
+
+    def back(self):
+        self.hide()
+        self.startwindow = CategorySelection()
+        self.startwindow.show()
+
+    def next(self):
+        self.hide()
+        self.diffwindow = DifficultySelection()
+        self.diffwindow.show()
+        print(selectedSubCategories)
+
+
+class DifficultySelection(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # for centering window
+        qtRectangle = self.frameGeometry()
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        qtRectangle.moveCenter(centerPoint)
+        self.move(qtRectangle.topLeft())
+
+        # init window
+        self.setWindowTitle("QuickBuzz")
+        self.setGeometry(qtRectangle)
+        self.setFixedWidth(600)
+        self.setFixedHeight(600)
+        self.setStyleSheet("background-color: #F9ADA0;")
+        self.setWindowIcon(QIcon("src/main/python/Icon.ico"))
+
+        # dropdowns for category and subcategory
+
+        self.diffDrop = QComboBox(self)
+        for i in difficulties:
+            self.diffDrop.addItem(i)
+
+        self.diffDrop.activated[str].connect(self.dropDownChangedDifficulty)
+        self.diffDrop.setFont(QFont("Helvetica Neue", 14))
+        self.diffDrop.setFixedWidth(370)
+        self.diffDrop.setStyleSheet("border: 5px black; background: #E4C1F9; height: 50px;")
+
+        self.rmDiff = QPushButton()
+        self.rmDiff.setText("Remove Difficulty")
+        self.rmDiff.setFont(QFont('Helvetica Neue', 12))
+        self.rmDiff.setFixedWidth(250)
+        self.rmDiff.setStyleSheet("QPushButton {"
+                                 "background-color: #FCF6BD;"
+                                 "border-radius: 15px; "
+                                 "padding: 5px; "
+                                 "border-style: outset;"
+                                 "margin: 5px"
+                                 "}"
+                                 "QPushButton:pressed {"
+                                 "background-color: #E4C1F9;"
+                                 "border-radius: 15px;"
+                                 "padding:5px;"
+                                 "border-style: inset;"
+                                 "}")
+        self.rmDiff.clicked.connect(self.removeDiff)
+        # navigation buttons
+        self.backButton = QPushButton()
+        self.backButton.setText("Back")
+        self.backButton.setFont(QFont('Helvetica Neue', 20))
+        self.backButton.setFixedWidth(200)
+        self.backButton.setStyleSheet("QPushButton {"
+                                      "background-color: #D0F4DE;"
+                                      "border-radius: 15px; "
+                                      "padding: 15px; "
+                                      "border-style: outset;"
+                                      "margin: 25px"
+                                      "}"
+                                      "QPushButton:pressed {"
+                                      "background-color: #A9DEF9;"
+                                      "border-radius: 15px;"
+                                      "padding:15px;"
+                                      "border-style: inset;"
+                                      "}")
+        self.backButton.clicked.connect(self.back)
+
+        self.nextButton = QPushButton()
+        self.nextButton.setText("Next")
+        self.nextButton.setFont(QFont('Helvetica Neue', 20))
+        self.nextButton.setFixedWidth(200)
+        self.nextButton.setStyleSheet("QPushButton {"
+                                      "background-color: #D0F4DE;"
+                                      "border-radius: 15px; "
+                                      "padding: 15px; "
+                                      "border-style: outset;"
+                                      "margin: 25px"
+                                      "}"
+                                      "QPushButton:pressed {"
+                                      "background-color: #A9DEF9;"
+                                      "border-radius: 15px;"
+                                      "padding:15px;"
+                                      "border-style: inset;"
+                                      "}")
+        self.nextButton.clicked.connect(self.next)
+
+        # putting it together
+        self.widget = QWidget(self)
+        self.mainLayout = QVBoxLayout(self.widget)
+
+        self.zeroHor = QHBoxLayout()
+        self.zeroHor.addWidget(self.rmDiff)
+
+        self.firstHor = QHBoxLayout()
+        self.firstHor.addWidget(self.diffDrop)
+
+        self.secondHor = QGridLayout()
+        self.gridx = 0
+        self.gridy = 0
+
+        self.thirdHor = QHBoxLayout()
+        self.thirdHor.addWidget(self.backButton)
+        self.thirdHor.addWidget(self.nextButton)
+
+        self.mainLayout.addLayout(self.zeroHor)
+        self.mainLayout.addLayout(self.firstHor)
+        self.mainLayout.addLayout(self.secondHor)
+        self.mainLayout.addLayout(self.thirdHor)
+        self.setCentralWidget(self.widget)
+
+    def dropDownChangedDifficulty(self, text):
+        self.diffDrop.setPlaceholderText(text)
+        if "Everything" not in text:
+            for i in range(self.diffDrop.count()):
+                if "Everything" in self.diffDrop.itemText(i):
+                    self.diffDrop.removeItem(i)
+
+
+
+
+        selectedDifficulties.append(text)
 
         self.tempButton = QPushButton()
         self.tempButton.setText(text)
@@ -552,19 +757,26 @@ class SubCategorySelection(QMainWindow):
         if self.gridy % 5 == 0 and self.gridy > 0:
             self.gridx += 1
             self.gridy = self.gridy % 5
-        selectedCategoryButtons.append(self.tempButton)
+        selectedDifficultiesButtons.append(self.tempButton)
 
-    def removeSubCat(self):
+    def removeDiff(self):
         try:
-            selectedSubCategoryButtons[len(selectedCategoryButtons)-1].hide()
-            selectedSubCategoryButtons.pop()
-            selectedSubCategories.pop()
+
+
+            selectedDifficultiesButtons[len(selectedDifficultiesButtons)-1].hide()
+            selectedDifficultiesButtons.pop()
+            selectedDifficulties.pop()
             if self.gridy != 0:
                 self.gridy -= 1
             elif self.gridy == 0 and self.gridx != 0:
                 self.gridx -= 1
                 self.gridy = 4
+
+
+            if selectedDifficulties == []:
+                self.diffDrop.addItem("Everything")
         except:
+
             catReply = QMessageBox()
             catReply.setIcon(QMessageBox.Warning)
             catReply.setWindowTitle("QuickBuzz")
@@ -576,24 +788,11 @@ class SubCategorySelection(QMainWindow):
                 print("ok")
 
 
-        self.categoryDrop.clear()
-        for i in subcategories:
-            if "Everything" in selectedSubCategories:
-                break
-            if i not in selectedSubCategories:
-                if i != "Everything":
-                    self.subCategoryDrop.addItem(i)
-
-
-    def dropDownChangedSubCategory(self, text):
-        self.subCategoryDrop.setPlaceholderText(text)
-        self.subCategoryDrop.adjustSize()
-
 
 
     def back(self):
         self.hide()
-        self.startwindow = CategorySelection()
+        self.startwindow = SubCategorySelection()
         self.startwindow.show()
 
     def next(self):
